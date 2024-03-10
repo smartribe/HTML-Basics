16bfb84e503fbc89118585f82b094f6fab2ac2ac
hagansamuel6 <hagansamuel6@gmail.com>
2024-03-10 17:51:10 +0000

    new files added

---
+++ javascript/code-challenge-2.js
@@ -0,0 +1,31 @@
+/*
+// Section 3 challenge
+// Ages
+let fidoAge, whiskersAge, dogYears, catYears;
+fidoAge = 12;
+whiskersAge = 9;
+dogYears = 7;
+catYears = 5;
+
+// Fido and Whiskers Age in Human Years
+let dogHumanYears, catHumanYears, isOlder;
+dogHumanYears = fidoAge * dogYears;
+
+console.log("Fido is " + dogHumanYears + " human years old.");
+
+catHumanYears = whiskersAge * catYears;
+
+console.log("Whiskers is " + catHumanYears + " human years old.");
+
+// is Fildo Older Than Whiskers?
+
+isOlder = dogHumanYears > catHumanYears;
+
+console.log("Is Fido Older?" + isOlder);
+*/
+
+// What is a Queryselector
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello World!";
+
+console.log(myHeading);
